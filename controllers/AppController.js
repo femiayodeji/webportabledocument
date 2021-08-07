ad3fe665aef3afb8f5d2e10f6d1ec95abd8b3839
Femi Ayodeji <femidotayodeji@gmail.com>
2021-08-08 00:51:49 +0100

    Add Base64 Conversion

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,18 +1,21 @@
-const fs = require("fs");
 const pdf = require('html-pdf');
 const options = {
-  format: "A3",
+  format: "A4",
   orientation: "portrait",
   border: "10mm",
 };
 
 exports.convertToPdf = (req, res) => {
-  // let html = req.body.htmlString;
-  let html = fs.readFileSync("./public/customer-order-invoice.html", "utf8");
-    pdf.create(html,options).toStream(function(err, stream){
-      // res.setHeader('Content-Length', stream.size);
-      res.setHeader('Content-Type', 'application/pdf');
-      res.setHeader('Content-Disposition', 'attachment; filename=invoice.pdf');
-      stream.pipe(res);
-    });
-  };
+  const base64String = req.body.base64String;
+  const htmlBuffer = Buffer.from(base64String, 'base64'); // Ta-da
+
+  const html = htmlBuffer.toString();
+  pdf.create(html,options).toStream(function(error, stream){
+    if(error != null)
+      res.status(500).send(error);
+
+    res.setHeader('Content-Type', 'application/pdf');
+    res.setHeader('Content-Disposition', 'attachment; filename=invoice.pdf');
+    stream.pipe(res);
+  });
+};
